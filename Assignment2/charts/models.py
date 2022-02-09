b36d888a2d5abf4a00b83f83ede1c2aaa3eb0316
Kamajuktus <45822450+Kamajuktus@users.noreply.github.com>
2022-02-09 18:13:24 +0600

    Add files via upload

---
+++ Assignment2/charts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+import requests
+
+
+class AccountInfo(models.Model):
+    account_name = models.CharField(max_length=50, null=False, blank=False)
+    account_summ = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.account_name} - {self.account_summ}'
